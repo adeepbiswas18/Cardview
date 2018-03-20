778f4d09b00301e7c7778297a0d72b4ce26bf946
adeep biswas <adeep.biswas12@gmail.com>
2018-03-20 16:50:44 +0530

    cardview done

---
+++ app/src/main/java/com/acm/cardview/album.java
@@ -0,0 +1,44 @@
+package com.acm.cardview;
+
+/**
+ * Created by adeep on 7/2/18.
+ */
+
+public class album {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public album() {
+    }
+
+    public album(String name, int numOfSongs, int thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
